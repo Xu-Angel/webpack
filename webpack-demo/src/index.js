bf5cd7df5f1db0b0ddca41edd4d7fa27d2397154
Angel <1049547382@qq.com>
2019-03-12 14:53:22 +0800

    管理输出：分离文件

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -2,11 +2,17 @@ import './style.css';
 import $ from './common/jQuery';
 import Icon from './common/1.png'
 import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
   element.innerText = 'hello webpack'
   element.classList.add('hello');
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
   var image = new Image()
   image.src = Icon
   element.appendChild(image)
