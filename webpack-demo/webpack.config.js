ad4bf7267716b7dc2389ed5cef362ec7d2a4522e
Angel <1049547382@qq.com>
2019-03-12 16:26:25 +0800

    TreeShaking-将文件标记为无副作用

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
   },
   module: {
     rules: [
+      // 在一个纯粹的 ESM 模块世界中，识别出哪些文件有副作用很简单。然而，我们的项目无法达到这种纯度，所以，此时有必要向 webpack 的 compiler 提供提示哪些代码是“纯粹部分”。
+      // {
+      //   include: path.resolve("node_modules", "lodash"),
+      //   sideEffects: false
+      // },
       {
         test: /\.css$/,
         use: [
