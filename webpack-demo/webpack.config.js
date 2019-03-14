26c63b35afa179b78313d72f99b6dc22e7e0f27c
Angel <1049547382@qq.com>
2019-03-14 20:31:19 +0800

    配置：html-webpack-plugin

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -12,69 +12,79 @@ module.exports = {
   // Use env.<YOUR VARIABLE> here:
   // console.log('Production: ', env.production) // true
 
-    mode: 'production',
-    devtool: 'inline-source-map', // 编译后的代码映射回原始源代码
-    devServer: { // 配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件。
-      contentBase: './dist',
-      hot: true // 是否开启热更新
-    },
-    plugins: [
-      new CleanWebpackPlugin(), // 清楚dist目录文件
-      new HtmlWebpackPlugin({ // 管理新生产的HTML 和 配置依赖
-        title: '热更新'
-      }),
-      new webpack.NamedModulesPlugin(), // 启用热更新 -- 更容易查看要修补(patch)的依赖
-      new webpack.HotModuleReplacementPlugin(), // 启用热更新
-      new ManifestPlugin() // 生成资源对应的JSON文件
-    ],
-    entry: {
-      app: './src/index.js',
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/' // 配合webpack-dev-middleware 使用资源 以确保文件资源能够在 http://localhost:3000 下正确访问，
-    },
-    module: {
-      rules: [{
-          test: /\.css$/,
-          /* 除了处理CSS外，借助于 style-loader 的帮助，CSS 的模块热替换实际上是相当简单的。当更新 CSS 依赖模块时，此 loader 在后台使用 module.hot.accept 来修补(patch) <style> 标签。 */
-          use: [
-            'style-loader',
-            'css-loader'
-          ]
-        },
-        {
-          test: /\.(png|svg|jpg|gif)$/,
-          use: [
-            'file-loader'
-          ]
-        },
-        /* file-loader 和 url-loader 可以接收并加载任何文件，然后将其输出到构建目录。这就是说，我们可以将它们用于任何类型的文件，包括字体 */
-        {
-          test: /\.(woff|woff2|eot|ttf|otf)$/,
-          use: [
-            'file-loader'
-          ]
-        },
-        /* 数据，如 JSON 文件，CSV、TSV 和 XML。类似于 NodeJS，JSON 支持实际上是内置的，也就是说 import Data from './data.json' 默认将正常运行。要导入 CSV、TSV 和 XML，你可以使用 csv-loader 和 xml-loader。 */
-        {
-          test: /\.(csv|tsv)$/,
-          use: [
-            'csv-loader'
-          ]
-        },
-        {
-          test: /\.xml$/,
-          use: [
-            'xml-loader'
-          ]
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "babel-loader"
-        }
-      ]
-    }
+  mode: 'production',
+  devtool: 'inline-source-map', // 编译后的代码映射回原始源代码
+  devServer: { // 配置告知 webpack-dev-server，在 localhost:8080 下建立服务，将 dist 目录下的文件，作为可访问文件。
+    contentBase: './dist',
+    hot: true // 是否开启热更新
+  },
+  plugins: [
+    new CleanWebpackPlugin(), // 清楚dist目录文件
+    new HtmlWebpackPlugin({ // 管理新生产的HTML 和 配置依赖
+      template: 'index.html',
+      templateParameters: {
+        param1: 'xxxx1',
+        param2: 'xxxx2',
+        title: '热更新',
+      },
+      minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseInlineTagWhitespace: true
+      }
+    }),
+    new webpack.NamedModulesPlugin(), // 启用热更新 -- 更容易查看要修补(patch)的依赖
+    new webpack.HotModuleReplacementPlugin(), // 启用热更新
+    new ManifestPlugin() // 生成资源对应的JSON文件
+  ],
+  entry: {
+    app: './src/index.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/' // 配合webpack-dev-middleware 使用资源 以确保文件资源能够在 http://localhost:3000 下正确访问，
+  },
+  module: {
+    rules: [{
+        test: /\.css$/,
+        /* 除了处理CSS外，借助于 style-loader 的帮助，CSS 的模块热替换实际上是相当简单的。当更新 CSS 依赖模块时，此 loader 在后台使用 module.hot.accept 来修补(patch) <style> 标签。 */
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      /* file-loader 和 url-loader 可以接收并加载任何文件，然后将其输出到构建目录。这就是说，我们可以将它们用于任何类型的文件，包括字体 */
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          'file-loader'
+        ]
+      },
+      /* 数据，如 JSON 文件，CSV、TSV 和 XML。类似于 NodeJS，JSON 支持实际上是内置的，也就是说 import Data from './data.json' 默认将正常运行。要导入 CSV、TSV 和 XML，你可以使用 csv-loader 和 xml-loader。 */
+      {
+        test: /\.(csv|tsv)$/,
+        use: [
+          'csv-loader'
+        ]
+      },
+      {
+        test: /\.xml$/,
+        use: [
+          'xml-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
 };
